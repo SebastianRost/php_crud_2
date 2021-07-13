7853afceca8cdfbe2b9bca456b860e6d70b5224c
sebastianrost <sebastianrost1990@gmail.com>
2021-07-13 00:12:13 -0300

    subido final del proyecto 13/7/21

--- view/MntProducto/mntproducto.js
+++ view/MntProducto/mntproducto.js
@@ -1,63 +1,63 @@
 var tabla;
 
-function init(){
-    $("#producto_form").on("submit",function(e){
-        guardaryeditar(e);	
+function init() {
+    $("#producto_form").on("submit", function (e) {
+        guardaryeditar(e);
     });
 }
 
-$(document).ready(function(){ 
-    tabla=$('#producto_data').dataTable({
-		"aProcessing": true,//Activamos el procesamiento del datatables
-	    "aServerSide": true,//Paginación y filtrado realizados por el servidor
-	    dom: 'Bfrtip',//Definimos los elementos del control de tabla
-	    buttons: [
-		            'copyHtml5',
-		            'excelHtml5',
-		            'csvHtml5',
-		            'pdf'
-		        ],
-        "ajax":{
+$(document).ready(function () {
+    tabla = $('#producto_data').dataTable({
+        "aProcessing": true,//Activamos el procesamiento del datatables
+        "aServerSide": true,//Paginación y filtrado realizados por el servidor
+        dom: 'Bfrtip',//Definimos los elementos del control de tabla
+        buttons: [
+            'copyHtml5',
+            'excelHtml5',
+            'csvHtml5',
+            'pdf'
+        ],
+        "ajax": {
             url: '../../controller/producto.php?op=listar',
-            type : "get",
-            dataType : "json",
-            error: function(e){
-                console.log(e.responseText);	
+            type: "get",
+            dataType: "json",
+            error: function (e) {
+                console.log(e.responseText);
             }
         },
-		"bDestroy": true,
-		"responsive": true,
-		"bInfo":true,
-		"iDisplayLength": 10,//Por cada 10 registros hace una paginación
-	    "order": [[ 0, "asc" ]],//Ordenar (columna,orden)
-	    "language": {
-            "sProcessing":     "Procesando...",
-            "sLengthMenu":     "Mostrar _MENU_ registros",
-            "sZeroRecords":    "No se encontraron resultados",
-            "sEmptyTable":     "Ningún dato disponible en esta tabla",
-            "sInfo":           "Mostrando un total de _TOTAL_ registros",
-            "sInfoEmpty":      "Mostrando un total de 0 registros",
-            "sInfoFiltered":   "(filtrado de un total de _MAX_ registros)",
-            "sInfoPostFix":    "",
-            "sSearch":         "Buscar:",
-            "sUrl":            "",
-            "sInfoThousands":  ",",
+        "bDestroy": true,
+        "responsive": true,
+        "bInfo": true,
+        "iDisplayLength": 10,//Por cada 10 registros hace una paginación
+        "order": [[0, "asc"]],//Ordenar (columna,orden)
+        "language": {
+            "sProcessing": "Procesando...",
+            "sLengthMenu": "Mostrar _MENU_ registros",
+            "sZeroRecords": "No se encontraron resultados",
+            "sEmptyTable": "Ningún dato disponible en esta tabla",
+            "sInfo": "Mostrando un total de _TOTAL_ registros",
+            "sInfoEmpty": "Mostrando un total de 0 registros",
+            "sInfoFiltered": "(filtrado de un total de _MAX_ registros)",
+            "sInfoPostFix": "",
+            "sSearch": "Buscar:",
+            "sUrl": "",
+            "sInfoThousands": ",",
             "sLoadingRecords": "Cargando...",
             "oPaginate": {
-                "sFirst":    "Primero",
-                "sLast":     "Último",
-                "sNext":     "Siguiente",
+                "sFirst": "Primero",
+                "sLast": "Último",
+                "sNext": "Siguiente",
                 "sPrevious": "Anterior"
             },
             "oAria": {
-                "sSortAscending":  ": Activar para ordenar la columna de manera ascendente",
+                "sSortAscending": ": Activar para ordenar la columna de manera ascendente",
                 "sSortDescending": ": Activar para ordenar la columna de manera descendente"
             }
-		}
-	}).DataTable();
+        }
+    }).DataTable();
 });
 
-function guardaryeditar(e){
+function guardaryeditar(e) {
     e.preventDefault();
     var formData = new FormData($("#producto_form")[0]);
     $.ajax({
@@ -66,7 +66,7 @@ function guardaryeditar(e){
         data: formData,
         contentType: false,
         processData: false,
-        success: function(datos){
+        success: function (datos) {
 
             $('#producto_form')[0].reset();
             $("#modalmantenimiento").modal('hide');
@@ -81,17 +81,18 @@ function guardaryeditar(e){
     });
 }
 
-function editar(prod_id){
-    $.post("../../controller/producto.php?op=mostrar",{prod_id:prod_id},function (data) {
+function editar(prod_id) {
+    $.post("../../controller/producto.php?op=mostrar", { prod_id: prod_id }, function (data) {
         data = JSON.parse(data);
         $('#prod_id').val(data.prod_id);
         $('#prod_nom').val(data.prod_nom);
+        $('#prod_desc').val(data.prod_desc);
     });
     $('#mdltitulo').html('Editar Registro');
     $('#modalmantenimiento').modal('show');
 }
 
-function eliminar(prod_id){
+function eliminar(prod_id) {
     swal.fire({
         title: 'CRUD',
         text: "Desea Eliminar el Registro?",
@@ -103,11 +104,11 @@ function eliminar(prod_id){
     }).then((result) => {
         if (result.isConfirmed) {
 
-            $.post("../../controller/producto.php?op=eliminar",{prod_id:prod_id},function (data) {
+            $.post("../../controller/producto.php?op=eliminar", { prod_id: prod_id }, function (data) {
 
             });
 
-            $('#producto_data').DataTable().ajax.reload();	
+            $('#producto_data').DataTable().ajax.reload();
 
             swal.fire(
                 'Eliminado!',
@@ -118,7 +119,7 @@ function eliminar(prod_id){
     })
 }
 
-$(document).on("click","#btnnuevo", function(){
+$(document).on("click", "#btnnuevo", function () {
     $('#prod_id').val('');
     $('#mdltitulo').html('Nuevo Registro');
     $('#modalmantenimiento').modal('show');
--- view/MntProducto/modalmantenimiento.php
+++ view/MntProducto/modalmantenimiento.php
@@ -1,4 +1,5 @@
-<div id="modalmantenimiento" class="modal fade bd-example-modal" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel" aria-hidden="true">
+<div id="modalmantenimiento" class="modal fade bd-example-modal" tabindex="-1" role="dialog"
+    aria-labelledby="myLargeModalLabel" aria-hidden="true">
     <div class="modal-dialog">
         <div class="modal-content">
             <form method="post" id="producto_form">
@@ -10,12 +11,20 @@
 
                     <div class="form-group">
                         <label class="form-label" for="prod_nom">Nombre</label>
-                        <input type="text" class="form-control" id="prod_nom" name="prod_nom" placeholder="Ingrese Nombre" required>
+                        <input type="text" class="form-control" id="prod_nom" name="prod_nom"
+                            placeholder="Ingrese Nombre" required>
+                    </div>
+                    <div class="form-group">
+                        <label class="form-label" for="prod_desc">Descripcion</label>
+                        <textarea class="form-control" placeholder="Ingrese Descripcion" rows="3" name="prod_desc"
+                            id="prod_desc" required></textarea>
+
                     </div>
                 </div>
                 <div class="modal-footer">
                     <button type="button" class="btn btn-rounded btn-default" data-dismiss="modal">Cerrar</button>
-                    <button type="submit" name="action" id="#" value="add" class="btn btn-rounded btn-primary">Guardar</button>
+                    <button type="submit" name="action" id="#" value="add"
+                        class="btn btn-rounded btn-primary">Guardar</button>
                 </div>
             </form>
         </div>
